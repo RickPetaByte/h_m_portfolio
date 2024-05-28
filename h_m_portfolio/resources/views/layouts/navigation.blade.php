115eca1b8e0a16a5e6413558b57858225605690d
Jur Heusschen <89153@roc-teraa.nl>
2024-05-28 17:18:26 +0200

    Custom scrollbar
    Custom scrollbar
    Light dark theme bugg fixes
    Responsive bugg fixes

--- h_m_portfolio/resources/views/layouts/navigation.blade.php
+++ h_m_portfolio/resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex btnHomepageNav">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-none">
                         <p class="colorFirst buttons"><i class="fa-solid fa-house iconNavBar iconNavBarColor"></i>{{ __('Homepage') }}</p>
                     </x-nav-link>
@@ -26,11 +26,12 @@
             </div>
             
             <div class="flex items-center">
+                <img src="img/moon.png" class="mr-5 icon iconThemeNavBar">
                 <!-- Check if user is authenticated -->
                 @if (Auth::check())
                     <!-- Settings Dropdown -->
                     <div class="hidden sm:flex sm:items-center sm:ml-6 colorSecond border border-0">
-                        <img src="img/moon.png" class="mr-5 icon">
+                        <!-- <img src="img/moon.png" class="mr-5 icon"> -->
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button class="buttons inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -73,15 +74,15 @@
                 @else
                     <!-- Links for guests -->
                     <div class="hidden sm:flex sm:items-center sm:ml-6 colorSecond">
-                        <img src="img/moon.png" class="mr-5 icon">
                         <a href="{{ route('login') }}" class="buttonsHome ml-4 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 focus:outline-none transition ease-in-out duration-150 colorFirst">Login</a>
                         <a href="{{ route('register') }}" class="buttonsHome ml-4 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 focus:outline-none transition ease-in-out duration-150 colorFirst">Register</a>
                     </div>
                 @endif
 
                 <!-- Hamburger -->
+                <!-- <img src="img/moon.png" class="mr-5 icon"> -->
                 <div class="-mr-2 flex items-center sm:hidden">
-                    <img src="img/moon.png" class="mr-5 icon">
+                    <!-- <img src="img/moon.png" class="mr-5 icon"> -->
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -96,7 +97,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="colorSecondHover">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="colorSecondHover homepageResponsiveColor">
                 <i class="fa-solid fa-house iconNavBar iconNavBarColor"></i>{{ __('Homepage') }}
             </x-responsive-nav-link>
         </div>
