8b2b3839c61927991d89f546c1ac725044da521e
Jur Heusschen <89153@roc-teraa.nl>
2024-06-05 19:26:36 +0200

    Another backend shit problem

--- h_m_portfolio/routes/web.php
+++ h_m_portfolio/routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\FileController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserTextController;
-// use App\Http\Controllers\PortfolioController;
-
 use App\Http\Controllers\PortfolioController;
 
 Route::get('/', function () {
@@ -18,24 +16,15 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-// Route::get('/portfolio-1', function () {
-//     return view('portfolio-1');
-// })->name('portfolio-1');
-
-// Route::get('/portfolio-2', function () {
-//     return view('portfolio-2');
-// })->name('portfolio-2');
-
-// Route::get('/portfolio-3', function () {
-//     return view('portfolio-3');
-// })->name('portfolio-3');
-
-// Route::get('/portfolio-4', function () {
-//     return view('portfolio-4');
-// })->name('portfolio-4');
-
 Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
 Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::middleware('auth')->group(function () {
 
@@ -44,8 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/{layout}', [PortfolioController::class, 'showPortfolio'])->name('portfolio.show.layout');
 
 
-
-
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
