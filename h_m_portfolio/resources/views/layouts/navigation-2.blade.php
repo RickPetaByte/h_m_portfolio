61cdc347e4b301a9c41cf7cb0e218878a79eb431
Rick <rmaas939@gmail.com>
2024-06-24 20:52:12 +0200

    Added bootstrap pop-up delete portfolio

--- h_m_portfolio/resources/views/layouts/navigation-2.blade.php
+++ h_m_portfolio/resources/views/layouts/navigation-2.blade.php
@@ -18,13 +18,70 @@
                 </div>
             </div>
 
-            <form action="{{ route('delete-portfolio') }}" method="POST" onsubmit="return confirm('Are you sure you want to delete the portfolio?');">
+            <form method="POST">
                 @csrf
                 <input type="hidden" name="file_name" value="{{ $fileName }}">
-                <button type="submit" class="btn btn-danger text-white fw-bold" id="deleteButton">
+                <button type="button" class="btn btn-danger text-white fw-bold" id="deleteButton" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <i class="fa fa-trash text-white mr-2"></i>Delete Portfolio
                 </button>
             </form>
+
+            <!-- Bootstrap pop-up -->
+            {{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete Portfolio</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Are you sure you want to delete the portfolio?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                                <i class="fa-solid fa-xmark text-white mr-2"></li>
+                                <span>No</span>
+                            </button>
+                            <form action="{{ route('delete-portfolio') }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="file_name" value="{{ $fileName }}">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-trash text-white mr-2"></li>
+                                    <span>Yes</span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete Portfolio</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <span>Are you sure you want to delete the portfolio?</span>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                                <i class="fa-solid fa-xmark text-white mr-2"></i>
+                                <span class="text-white">No</span>
+                            </button>
+                            <form action="{{ route('delete-portfolio') }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="file_name" value="{{ $fileName }}">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-trash text-white mr-2"></i>
+                                    <span class="text-white ">Yes</span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="flex items-center">
                 <img src="img/moon.png" class="mr-5 icon iconThemeNavBar">
